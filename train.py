fa98aeedc45c1cc6f7d5ced57ab463bb8050f193
logan-hartford <91397886+logan-hartford@users.noreply.github.com>
2024-10-22 16:02:26 -0400

    indoor images

--- train.py
+++ train.py
@@ -15,9 +15,9 @@ def get_next_save_filename(base_name="model", extension=".pt", directory=save_di
 
     return os.path.join(directory, f"{base_name}{next_number}{extension}")
 
-datapath = "coco8.yaml"
+# datapath = "coco8.yaml"
 # datapath = "D:\Documents\GitHub\weedy_images\datasets\oct-16-unaugmented\data.yaml"
-# datapath = "D:\Documents\GitHub\weedy_images\datasets\oct16-augmented\data.yaml"
+datapath = "D:\Documents\GitHub\weedy_images\datasets\oct16-augmented\data.yaml"
 
 model_path = "models/yolo11n.pt"
 # model_path = "models/saves/100epochs.pt"
@@ -30,26 +30,25 @@ if __name__ == '__main__':
         # Train model with data augmentation parameters (default values)
         results = model.train(
             data=datapath,
-            epochs=10,
+            epochs=1000,
             device=0,
-            patience=300,
             # Data augmentation hyperparameters
-            hsv_h=0.015,                    # Adjust hue | default: 0.015
-            hsv_s=0.7,                      # Adjust saturation | default: 0.7
-            hsv_v=0.4,                      # Adjust brightness (value) | default: 0.4
-            degrees=0.0,                    # Rotate image | default: 0.0
+            hsv_h=0.02,                    # Adjust hue | default: 0.015
+            hsv_s=0.8,                      # Adjust saturation | default: 0.7
+            hsv_v=0.5,                      # Adjust brightness (value) | default: 0.4
+            degrees=5.0,                    # Rotate image | default: 0.0
             translate=0.1,                  # Translate image | default: 0.1
-            scale=0.5,                      # Scale image | default: 0.5
-            shear=0.0,                      # Shear image | default: 0.0
+            scale=0.7,                      # Scale image | default: 0.5
+            shear=0.1,                      # Shear image | default: 0.0
             perspective=0.0,                # Perspective warp image | default: 0.0
             flipud=0.0,                     # Flip image upside down probability | default: 0.0
             fliplr=0.5,                     # Flip image left to right probability | default: 0.5
             bgr=0.0,                        # Randomly change to BGR order | default: 0.5
             mosaic=1.0,                     # Mosaic augmentation | default: 1.0
-            mixup=0.0,                      # Mixup augmentation | default: 0.0
-            copy_paste=0.0,                 # Copy-paste augmentation | default: 0.0
+            mixup=0.1,                      # Mixup augmentation | default: 0.0
+            copy_paste=0.1,                 # Copy-paste augmentation | default: 0.0
             copy_paste_mode="flip",         # Copy-paste mode | default: "flip"
-            auto_augment="randaugment",     # Auto augment policy | default: "randaugment"
+            auto_augment="trivialaugment",     # Auto augment policy | default: "randaugment"
             erasing=0.4,                    # Random erasing probability | default: 0.4
             crop_fraction=1.0               # Fraction of image to crop | default: 1.0
         )
