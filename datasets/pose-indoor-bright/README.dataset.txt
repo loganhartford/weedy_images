e0cdb5fb465e48f9bd725eefaa3f4400dcb424a4
logan-hartford <91397886+logan-hartford@users.noreply.github.com>
2024-11-22 17:32:17 -0500

    brightness indoor

---
+++ datasets/pose-indoor-bright/README.dataset.txt
@@ -0,0 +1,6 @@
+# dandelion_keypoints > indoor-brightness
+https://universe.roboflow.com/weedy/dandelion_keypoints
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import yaml
 # img_path = "D:/Documents/GitHub/weedy_images/datasets/oct16-augmented/test/images/captured_image_20241016_153912_jpg.rf.35894ba5438fabbbc9a7650da1e6ba62.jpg"
 
 model_path = "D:\Documents\GitHub\weedy_images\models\indoor_pose_ncnn_model"
-img_path = "D:\Documents\GitHub\weedy_images\img\oct-22-indoor\captured_image_20241022_152954.jpg"
+img_path = "D:\Documents\GitHub\weedy_images\img\oct-22-indoor\captured_image_20241022_155626.jpg"
 yaml_file_path = 'D:\Documents\GitHub\weedy_images\models\indoor_pose_ncnn_model\metadata.yaml'
 
 keys = [
@@ -33,21 +33,25 @@ if __name__ == '__main__':
         #results.show()
         result.save()
         # print(result.keypoints[0].info())
+        # print(result.keypoints.has_visible)
+        # print(result.keypoints[0].has_visible)
 
         keypoints = []
         for i, keypoint in enumerate(result.keypoints):
             data = keypoint.data
-            print(f"Keypoint data: {data}")
+            print(f"Keypoint {i}: {keypoint}")
+            print(f"data: {data[0]}")
 
-            keypoint_dict = {
-                "name": names[0],
-                "flower": tuple(round(x, 3) for x in data[i][0].tolist()),
-                "base": tuple(round(x, 3) for x in data[i][1].tolist()),
-                "upper": tuple(round(x, 3) for x in data[i][2].tolist()),
-                "lower": tuple(round(x, 3) for x in data[i][3].tolist()),
-            }
+            if keypoint.has_visible:
+                print(f"Keypoint {i} is visible.")
 
-            print(keypoint_dict)
+            # keypoint_dict = {
+            #     "flower": tuple(round(x, 3) for x in data[i][0].tolist()),
+            #     "base": tuple(round(x, 3) for x in data[i][1].tolist()),
+            #     "upper": tuple(round(x, 3) for x in data[i][2].tolist()),
+            #     "lower": tuple(round(x, 3) for x in data[i][3].tolist()),
+            # }
+            # keypoints.append(keypoint_dict)
 
         # for box in result.boxes:
         #     xyxy = box.xyxy[0].cpu()  # Get [xmin, ymin, xmax, ymax] format
